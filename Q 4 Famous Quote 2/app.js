559a25df631a67f78cc542b1a064186a0a7a62fb
SabeenAkhtar <sabeen.ilmesters2@gmail.com>
2024-06-05 07:00:36 -0700

    first commit

---
+++ Q 4 Famous Quote 2/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new. ";
+let Famous_Person = "Albert, Einstein";
+let message = `${Famous_Person} Once said,"${quote}"`;
+console.log(message);
