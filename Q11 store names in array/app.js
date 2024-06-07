58aaa67b05e06fe79f88edcb857ce8d1e8f34955
SabeenAkhtar <sabeen.ilmesters2@gmail.com>
2024-06-07 03:48:06 -0700

    first commit

---
+++ Q11 store names in array/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Q11 - Names: Store the names of a few of your friends in a array called names. 
+//Print each person’s name by accessing each element in the list, one at a time.
+let Friends = ['Sara', 'Zara', 'Maya', 'Baya', 'Haya',];
+for (let i = 0; i < Friends.length; i++) {
+    console.log(Friends[i]);
+}
---
+++ Q11 store names in array/app.ts
@@ -0,0 +1,7 @@
+// Q11 - Names: Store the names of a few of your friends in a array called names. 
+//Print each person’s name by accessing each element in the list, one at a time.
+
+let Friends:string[] =['Sara','Zara','Maya', 'Baya','Haya',];
+for (let i=0; i< Friends.length; i ++ ){
+    console.log(Friends[i]);
+}
